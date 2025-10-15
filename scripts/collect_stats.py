e6ff9391caa66bf60943efdf0402438f3fce7339
Robin Coutelier <robin.coutelier@gmail.com>
2025-10-15 20:29:34 +0200

    better partial save

--- scripts/collect_stats.py
+++ scripts/collect_stats.py
@@ -119,6 +119,7 @@ if __name__ == "__main__":
     start = time.time()
     total_jobs = len(jobs)
     instances_solved = 0
+    last_save = 0
     with Progress() as progress:
         task = progress.add_task(f"Collecting stats | 0/{total_jobs}", total=total_jobs)
         while len(jobs) > 0 or len(thread_pool) > 0:
@@ -141,18 +142,18 @@ if __name__ == "__main__":
             avg_time = 0
             if n_completed > 0:
                 avg_time = df["Time (ms)"].mean()
+
+            if instances_solved - last_save >= 100:
+                # sort the dataframe by file name and option
+                df = df.sort_values(by=["file", "option"])
+                # save the dataframe to a csv file
+                df.to_csv(out_file, index=False)
+                last_save = instances_solved
             mutex.release()
 
             # sleep for avg_time / 100 (expressed in seconds, but the stat is in milliseconds)
             time.sleep(avg_time / 10**5 if avg_time > 0 else 0.00001)
 
-            if instances_solved % 100 == 0:
-                mutex.acquire()
-                # sort the dataframe by file name and option
-                df = df.sort_values(by=["file", "option"])
-                # save the dataframe to a csv file
-                df.to_csv(out_file, index=False)
-                mutex.release()
 
     # drop the unnecessary columns from the dataframe
     df = df.drop(columns=["Variable added", "Backtracking started", "Invariants checked", "Allocated Chunk", "Purging clauses", "Binary clause simplified", "Literal removed from clause"], errors="ignore")
